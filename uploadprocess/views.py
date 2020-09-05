380aba5123e8fd9e0d0950d2c97c917be56663d9
Sahil <sjha200000@gmail.com>
2020-09-05 12:31:23 +0530

    added documentation

--- uploadprocess/views.py
+++ uploadprocess/views.py
@@ -7,7 +7,11 @@ import os
 import io
 
 from .forms import FileUpload
+"""
 
+API to upload files, writes the uploaded file in upload_files directory
+
+"""
 def upload(request):
 	if request.method == 'POST':
 		upload_form = FileUpload(request.POST,request.FILES)
@@ -23,20 +27,29 @@ def upload(request):
 		upload_form = FileUpload()
 	return render(request,'upload.html',{'form':upload_form})	
 
+"""
+API To implement first task
+params: upload_file/data
+return: Zip with three child datasets
+
+"""
 def subset(request,filename):
 	df = pd.read_excel('upload_files/'+filename+'.xlsx')
-
+	# filtering out all the data for metabolite ids ending with: "PC"
 	pc_df = df[(df['Accepted Compound ID'].str.endswith('PC',na=False)) & (df['Accepted Compound ID'].str[-3]!='L')]
 	pc_df.to_csv('PC.csv',index=False)
 
+	# filtering out all the data for metabolite ids ending with: "LPC"
 	lpc_df = df[df['Accepted Compound ID'].str.endswith('LPC',na=False)]
 	lpc_df.to_csv('LPC.csv',index=False)
 
+	# filtering out all the data for metabolite ids ending with: "plasmogen"
 	plasgn_df = df[df['Accepted Compound ID'].str.endswith('plasmalogen',na=False)]
 	plasgn_df.to_csv('Plasmalogen.csv',index=False)
 
 	zip_file_list = ['PC.csv','LPC.csv','Plasmalogen.csv']
 	
+	#zipping three child datsets named, 'PC.csv', 'LPC.csv', 'Plasmogen.csv'
 	download_file = io.BytesIO()
 	with ZipFile(download_file,'w') as zip_file:
 		for csv_file in zip_file_list:
@@ -48,6 +61,11 @@ def subset(request,filename):
 			
 	return response
 
+"""
+Helper function to round off values in Retention Time Roundoff (in mins).
+
+"""
+
 def rounded_time(row):
 	time = row['Retention time (min)'] 
 	if time>1:
@@ -55,17 +73,29 @@ def rounded_time(row):
 	else:
 		return 1	
 
+"""
+API To implement second and third task
+params: upload_file/data
+return: Zip with two files for task2 and task3 respectively
+
+"""
+
 def grouping(request, filename):
   df = pd.read_excel('upload_files/'+filename+'.xlsx')
-	
+
+  # return the dataset with new column "Retention Time Roundoff (in mins)"	
   rounded_retention_time = df.apply(lambda row : rounded_time(row),axis=1)
   df.insert(2,'Retention Time Roundoff (in mins)',rounded_retention_time)
   
   df.to_csv('task2.csv',index=False)
-
+  
+  #dropping columns 'm/z', 'Retention time (min)' and 'Accepted Compound ID' for third task
   del df['m/z']
+  del df['Accepted Compound ID']
   del df['Retention time (min)']
-
+  
+  #finding the mean of all metabolites which have same "Rentention Time Roundoff" across all the samples 
+  #and the resultant is new dataframe which includes "Retention Time Roundoff" and all samples.
   dfsorted=df.sort_values('Retention Time Roundoff (in mins)')
   avg = dfsorted.groupby('Retention Time Roundoff (in mins)').mean().reset_index()
   avg['avg']=avg.mean(axis=1)
@@ -74,7 +104,8 @@ def grouping(request, filename):
   zip_file_list = ['task2.csv', 'task3.csv']
 
   download_file = io.BytesIO()
-
+  
+  #grouping the outputs in zipfile 
   with ZipFile(download_file,'w') as zip_file:
 	  for csv_file in zip_file_list:
 		  zip_file.write(csv_file)
